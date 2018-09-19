c91607f116c315ad191d24889005a91b50c00769
Taylor Jackson Disalvo <taylor.disalvo@stu.jefferson.kyschools.us>
2018-09-19 09:38:38 -0400

    Second Python GitHub project.

--- Dictionaries In Action.py
+++ Dictionaries In Action.py
@@ -1,53 +1,77 @@
 dictionary = {
     'wallet' : 0,
+    'walletTotal' : 0,
     'cost' : 0,
+    'costTotal' : 0,
+    'endSum' : 0,
     'gone' : "",
 }
+
 
 def shopping():
     dictionary['gone'] = input("Would you like to go out? 'y'es or 'n'o\n")
     if dictionary['gone'].lower() == "y":
         print("You go to your car and begin the journey.")
         out()
+        
     elif dictionary['gone'].lower() == "n":
         print("You decide to stay home like an intelligent individual.\n")
         pass
+        
     else:
         print("Please input a valid command.")
         shopping()
+        
 
 def out():
     print("You have arrived at the store.")
     while dictionary['gone'] == "y":
         x = input("\nWhat would you like to do now? 'a'tm, 'c'heckout, 'g'o home\n")
         if x.lower() == "a":
-            aa = input("You walk to the nearest ATM. How much would you like to "
-                       "withdraw?\nPUT IN A NUMBER\n")
+            aa = input("You walk to the nearest ATM. How much would you like "
+                       "to withdraw?\nENTER A NUMBER\n")
             dictionary['wallet'] = dictionary['wallet'] + int(aa)
+            dictionary['walletTotal'] = dictionary['walletTotal'] + int(aa)
             print("You now have $%d in your wallet."% (dictionary['wallet']))
             
         elif x.lower() == "c":
-            cc = input("You have arrived at the checkout counter. How much are "
-                       "you spending?\nPUT IN A NUMBER\n")
-            dictionary['cost'] -= int(cc)
+            dictionary['cost'] = input("You have arrived at the checkout "
+                                       "counter. How much are you spending?"
+                                       "\nENTER A NUMBER\n")
+            dictionary['costTotal'] += int(dictionary['cost'])
             afford()
             
-        elif x.lower() == "g": 
+        elif x.lower() == "g":
+            dictionary['endSum'] = dictionary['walletTotal'] - dictionary['costTotal']
             print("You decide to call it quits; It's time to go home.")
             dictionary['gone'] = "n"
-            print("Today you spent a total of $%d."% (dictionary['cost']))
-            pass
+            print("Today, you withdrew $%d from your account and spent a "
+                  "total of $%d."%
+                  (dictionary['walletTotal'], dictionary['costTotal']))
+            
+            if dictionary['endSum'] < 0:
+                print("Somehow, you managed to go into debt... $%d, to be "
+                      "exact. Never go shopping alone again."%
+                      (dictionary['endSum']))
+                
+            print("This was the end dictionary:\n" , dictionary)
+            
         else:
             print("Please input a valid command.")
             out()
 
+
 def afford():
     print("You're wallet is holding $%d, and you want to spend $%d."%
-          (dictionary['wallet'], dictionary['cost']))
-    if dictionary['wallet'] + dictionary['cost'] > 0:
-        print("You can afford to make this purchase.")
-        dictionary['wallet'] = dictionary['wallet'] - dictionary['cost']
-    elif dictionary['wallet'] + dictionary['cost'] < 0:
-        print("You cannot afford to make this purchase.")
+          (dictionary['wallet'], int(dictionary['cost'])))
+    if dictionary['wallet'] - int(dictionary['cost']) > 0:
+        print("You make the purchase.\n")
+        dictionary['wallet'] = dictionary['wallet'] - int(dictionary['cost'])
+        print("Your wallet now contains $%d."% (dictionary['wallet']))
+        
+    elif dictionary['wallet'] - int(dictionary['cost']) < 0:
+        print("You cannot afford to make this purchase.\n")
+        print("Your wallet still contains $%d."% (dictionary['wallet']))
+        
 
 shopping()
