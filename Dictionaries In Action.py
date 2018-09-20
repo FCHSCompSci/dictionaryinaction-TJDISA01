7a7ead5f270e398115f771790155803248a7f396
Taylor Jackson Disalvo <taylor.disalvo@stu.jefferson.kyschools.us>
2018-09-20 09:39:01 -0400

    Second Python GitHub project. (Final commit)

--- Dictionaries In Action.py
+++ Dictionaries In Action.py
@@ -7,21 +7,16 @@ dictionary = {
     'gone' : "",
 }
 
-
 def shopping():
     dictionary['gone'] = input("Would you like to go out? 'y'es or 'n'o\n")
     if dictionary['gone'].lower() == "y":
         print("You go to your car and begin the journey.")
         out()
-        
     elif dictionary['gone'].lower() == "n":
         print("You decide to stay home like an intelligent individual.\n")
-        pass
-        
     else:
         print("Please input a valid command.")
         shopping()
-        
 
 def out():
     print("You have arrived at the store.")
@@ -33,45 +28,47 @@ def out():
             dictionary['wallet'] = dictionary['wallet'] + int(aa)
             dictionary['walletTotal'] = dictionary['walletTotal'] + int(aa)
             print("You now have $%d in your wallet."% (dictionary['wallet']))
-            
         elif x.lower() == "c":
             dictionary['cost'] = input("You have arrived at the checkout "
                                        "counter. How much are you spending?"
                                        "\nENTER A NUMBER\n")
             dictionary['costTotal'] += int(dictionary['cost'])
             afford()
-            
         elif x.lower() == "g":
-            dictionary['endSum'] = dictionary['walletTotal'] - dictionary['costTotal']
-            print("You decide to call it quits; It's time to go home.")
-            dictionary['gone'] = "n"
-            print("Today, you withdrew $%d from your account and spent a "
-                  "total of $%d."%
-                  (dictionary['walletTotal'], dictionary['costTotal']))
-            
-            if dictionary['endSum'] < 0:
-                print("Somehow, you managed to go into debt... $%d, to be "
-                      "exact. Never go shopping alone again."%
-                      (dictionary['endSum']))
-                
-            print("This was the end dictionary:\n" , dictionary)
-            
+            when_g()
         else:
             print("Please input a valid command.")
-            out()
-
 
 def afford():
     print("You're wallet is holding $%d, and you want to spend $%d."%
           (dictionary['wallet'], int(dictionary['cost'])))
-    if dictionary['wallet'] - int(dictionary['cost']) > 0:
-        print("You make the purchase.\n")
-        dictionary['wallet'] = dictionary['wallet'] - int(dictionary['cost'])
-        print("Your wallet now contains $%d."% (dictionary['wallet']))
-        
-    elif dictionary['wallet'] - int(dictionary['cost']) < 0:
-        print("You cannot afford to make this purchase.\n")
-        print("Your wallet still contains $%d."% (dictionary['wallet']))
-        
+    dictionary['wallet'] = dictionary['wallet'] - int(dictionary['cost'])
+    print("You make the purchase.\n")
+    if dictionary['wallet'] > 0:
+        print("In your wallet remains $%d."% (dictionary['wallet']))
+    elif dictionary['wallet'] < 0:
+        print("You didn't have enough money in your wallet.\nYou will now owe "
+              "$%d to your bank.\n"% (abs(dictionary['wallet'])))
 
+def when_g():
+    dictionary['endSum'] = dictionary['walletTotal'] - dictionary['costTotal']
+    print("You decide to call it quits; It's time to go home.")
+    dictionary['gone'] = "n"
+    print("Today, you withdrew $%d from your account and spent a "
+          "total of $%d."% (dictionary['walletTotal'], dictionary['costTotal']))
+    if dictionary['endSum'] > 0:
+        print("You go home happy, with money in your pocket.")
+    elif dictionary['endSum'] < 0:
+        print("Somehow, you managed to go into debt... $%d, to be "
+              "exact. Never go shopping alone again."% (dictionary['endSum']))   
+    print("This was the end dictionary:\n")
+    for k, v in dictionary.items():
+        print(k + ":", v)
+    dictionary['wallet'] = 0
+    dictionary['walletTotal'] = 0
+    dictionary['cost'] = 0
+    dictionary['costTotal'] = 0
+    dictionary['endSum'] = 0
+    dictionary['gone'] = ""
+            
 shopping()
